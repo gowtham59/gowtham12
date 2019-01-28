be3fb8eb20a04ed7814f62be6804d17b63f6fdfc
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-01-28 23:07:52 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int m,n,o;
+scanf("%d%d%d",&m,&n,&o);
+if(m>n)
+{
+if(m>o)
+printf("%d is greatest",m);
+else
+printf("%d is greatest",o);
+}
+else
+{
+if(n>o)
+printf("%d is greatest",n);
+else
+printf("%d is greatest",o);
+}
+}
