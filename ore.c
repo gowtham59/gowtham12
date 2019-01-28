5a9a6103574f6bfcfbf5f28430dcdc97bfbe0b85
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-01-28 21:51:16 +0530

    Create ore.c

---
+++ ore.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter the number:");
+scanf("%d",a)
+if(a%2==0)
+{
+printf("The given number is Even");
+}
+else if(a%2!=0)
+{
+printf("The given number is Even");
+}
+else
+{
+printf("Invalid input");
+}
+getch();
+}
